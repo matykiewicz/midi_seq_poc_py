dbab7810c3d2ad6b9ce2e1fa0fb0c6754ff9147e
Entaroadun <pawel.matykiewicz@gmail.com>
2024-04-08 14:08:25 -0700

    Fixing midi in and out message handling data structures

--- midi_seq_txt/presets.py
+++ midi_seq_txt/presets.py
@@ -7,20 +7,22 @@ from typing import Any, Dict, List, Set, Tuple, Type, Union
 import attrs
 import yaml
 
-from .functionalities import MOutFunctionality, MMappings, MMusic
+from .functionalities import MOutFunctionality, MMappings, MMusic, MInFunctionality
 
-PRESET_TYPES: Dict[str, Union[Type[MOutFunctionality], Type[MMappings], Type[MMusic]]] = {
+PRESET_TYPES: Dict[str, Union[Type[MOutFunctionality], Type[MMappings], Type[MMusic], Type[MInFunctionality]]] = {
     "MMappings": MMappings,
     "MOutFunctionality": MOutFunctionality,
+    "MInFunctionality": MInFunctionality,
     "MMusic": MMusic,
 }
 
 
 def read_all_presets(
     args: Namespace,
-) -> Tuple[List[MOutFunctionality], Set[str], List[MMappings], List[MMusic]]:
+) -> Tuple[List[MOutFunctionality], Set[str], List[MInFunctionality], List[MMappings], List[MMusic]]:
     loc: str = args.dir
     all_modes: List[MOutFunctionality] = list()
+    all_messages: List[MInFunctionality] = list()
     all_mappings: List[MMappings] = list()
     all_music: List[MMusic] = list()
     all_instruments: Set[str] = set()
@@ -33,6 +35,10 @@ def read_all_presets(
             all_modes.append(mode)
             for instrument in mode.instruments:
                 all_instruments.add(instrument)
+        elif class_name == "MInFunctionality":
+            message_dict = read_preset(file_path=file_path)
+            message = MInFunctionality(**message_dict)
+            all_messages.append(message)
         elif class_name == "MMappings":
             mappings_dict = read_preset(file_path=file_path)
             mapping = MMappings(**mappings_dict)
@@ -41,7 +47,7 @@ def read_all_presets(
             music_dict = read_preset(file_path=file_path)
             music = MMusic(**music_dict)
             all_music.append(music)
-    return all_modes, all_instruments, all_mappings, all_music
+    return all_modes, all_instruments, all_messages, all_mappings, all_music
 
 
 def read_preset(file_path: str) -> Dict[str, Any]:
@@ -58,7 +64,7 @@ def read_preset_type(file_path: str) -> Union[MMappings, MOutFunctionality, MMus
     preset_type: Union[Type[MOutFunctionality], Type[MMappings], Type[MMusic]] = PRESET_TYPES[
         class_name
     ]
-    return preset_type(**preset_dict)
+    return preset_type(**preset_dict)  # noqa
 
 
 def write_preset_type(preset: Union[MMappings, MOutFunctionality, MMusic], loc: str) -> None:
@@ -81,7 +87,7 @@ def write_all_presets(args: Namespace) -> None:
     for obj_name in dir(midi_seq_txt.init):
         if "_" in obj_name:
             obj = getattr(midi_seq_txt.init, obj_name)
-            if obj.__class__.__name__ in ["MMappings", "MOutFunctionality", "MMusic"]:
+            if obj.__class__.__name__ in ["MMappings", "MOutFunctionality", "MMusic", "MInFunctionality"]:
                 presets.append(obj)
     for preset in presets:
         write_preset_type(preset=preset, loc=loc)
